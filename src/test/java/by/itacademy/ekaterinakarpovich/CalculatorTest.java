6ffcb2ad2d24c32029dd7251717f9ed19dc4d896
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-17 17:15:41 +0300

    add test testAdditionPositiveNumbers

--- src/test/java/by/itacademy/ekaterinakarpovich/CalculatorTest.java
+++ src/test/java/by/itacademy/ekaterinakarpovich/CalculatorTest.java
@@ -1,4 +1,14 @@
 package by.itacademy.ekaterinakarpovich;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAdditionPositiveNumbers() {
+        assertEquals(10, calculator.add(2, 8), "Сумма должна быть равна 10");
+    }
 }
